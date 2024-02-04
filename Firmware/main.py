e606a9f263bc05696d5cc7255833ecdb38a4eac4
sam-jam-sam <samuelscottmiller@gmail.com>
2024-02-04 16:06:56 +1100

    Implement control of onboard neopixel

--- Firmware/main.py
+++ Firmware/main.py
@@ -4,6 +4,10 @@ import board
 
 from kmk.extensions.media_keys import MediaKeys
 
+from kmk.extensions.RGB import RGB
+
+
+
 from kmk.modules.encoder import EncoderHandler
 encoder_handler = EncoderHandler()
 
@@ -13,6 +17,9 @@ from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
 
 keyboard = KMKKeyboard()
+
+rgb = RGB(pixel_pin=board.NEOPIXEL, num_pixels=1)
+keyboard.extensions.append(rgb)
 
 keyboard.col_pins = (board.D2,board.D5,board.D6)
 keyboard.row_pins = (board.D3,board.D4)
@@ -25,7 +32,7 @@ encoder_handler.pins = ( (board.D9, board.D8, board.D7,),
 )
 
 keyboard.keymap = [
-    [KC.LGUI(KC.N6), KC.MEDIA_PREV_TRACK, KC.MEDIA_NEXT_TRACK, KC.LCTRL(KC.LGUI(KC.N3)), KC.LGUI(KC.N4), KC.LGUI(KC.N5)]
+    [KC.RGB_MODE_BREATHE_RAINBOW, KC.MEDIA_PREV_TRACK, KC.MEDIA_NEXT_TRACK, KC.LCTRL(KC.LGUI(KC.N3)), KC.LGUI(KC.N4), KC.LGUI(KC.N5)]
 ]
 
 encoder_handler.map = [ (( KC.VOLU, KC.VOLD, KC.MEDIA_PLAY_PAUSE),), 
